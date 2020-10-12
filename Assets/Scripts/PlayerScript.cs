7d27d08691cefad2f6039dee41f946d3dc2f5bc6
man1230 <marcusnapoleoni@knights.ucf.edu>
2020-10-11 20:46:21 -0400

    First commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    public float speed = 10f;
+
+    public Text score;
+
+    private int scoreValue = 0;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        score.text = scoreValue.ToString();
+    }
+
+    void FixedUpdate ()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+
+        rb2d.AddForce(new Vector2(hozMovement * speed * Time.deltaTime, vertMovement * speed * Time.deltaTime));
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Coin")
+        {
+            scoreValue++;
+            score.text = scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
