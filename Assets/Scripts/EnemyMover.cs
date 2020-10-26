7e9b755c8d8409cb27e103cd61963af8879fdb98
man1230 <marcusnapoleoni@knights.ucf.edu>
2020-10-25 23:42:29 -0400

    Challenge commit

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+// Transforms to act as start and end markers for the journey.
+private Vector3 pos1;
+private Vector3 pos2;
+public Vector3 offset = new Vector3 (10f, 0f, 0f);
+public float speed = 1.0f;
+
+// Time when the movement started.
+private float startTime;
+
+// Total distance between the markers.
+private float journeyLength;
+
+void Start()
+     {
+         pos1 = transform.position;
+         pos2 = transform.position + offset;
+     // Keep a note of the time the movement started.
+          startTime = Time.time;
+
+     // Calculate the journey length.
+          journeyLength = Vector3.Distance(pos1, pos2);
+     }
+
+// Follows the target position like with a spring
+void Update()
+     {
+     // Distance moved = time * speed.
+          float distCovered = (Time.time - startTime) * speed;
+
+     // Fraction of journey completed = current distance divided by total distance.
+          float fracJourney = distCovered / journeyLength;
+
+     // Set our position as a fraction of the distance between the markers and pingpong the movement.
+          transform.position = Vector3.Lerp(pos1, pos2, Mathf.PingPong (fracJourney, 1));
+     }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -11,12 +11,41 @@ public class PlayerScript : MonoBehaviour
 
     public Text score;
 
+    public Text win;
+
+    public Text madeBy;
+
+    public Text lives;
+
     private int scoreValue = 0;
+
+    private int numLives = 3;
+
+    public AudioSource musicSource;
+    public AudioClip music;
+    public AudioClip winSound;
+
+    Animator anim;
+    private bool facingRight = true;
+
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
 
     void Start()
     {
+        musicSource.clip = music;
+        musicSource.loop = true;
+        musicSource.Play();
+
+        anim = GetComponent<Animator>();
+
         rb2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        lives.text = "Lives: " + numLives.ToString();
+        win.text = "";
+        madeBy.text = "";
     }
 
     void FixedUpdate ()
@@ -24,11 +53,32 @@ public class PlayerScript : MonoBehaviour
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
 
+         isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+
         rb2d.AddForce(new Vector2(hozMovement * speed * Time.deltaTime, vertMovement * speed * Time.deltaTime));
 
         if (Input.GetKey("escape"))
         {
             Application.Quit();
+        }
+
+        if (isOnGround == false)
+        {
+            anim.SetInteger("State", 2);
+        } else if (hozMovement != 0)
+        {
+            anim.SetInteger("State", 1);
+        } else if (hozMovement == 0)
+        {
+            anim.SetInteger("State", 0);
+        }
+
+            if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        } else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
         }
     }
 
@@ -39,17 +89,59 @@ public class PlayerScript : MonoBehaviour
             scoreValue++;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+
+            if (scoreValue == 4)
+            {
+                this.transform.position = new Vector3 (50, 0, 0);
+
+                numLives = 3;
+                lives.text = "Lives: " + numLives.ToString();
+            }
+
+
+            if (scoreValue == 8)
+            {
+                win.text = "You Win!!!";
+                madeBy.text = "Made by Marcus Napoloni";
+
+                musicSource.clip = winSound;
+                musicSource.loop = false;
+                musicSource.Play();
+            }
+        }
+
+        if (collision.collider.tag == "Enemy")
+        {
+            numLives--;
+            lives.text = "Lives: " + numLives.ToString();
+            Destroy(collision.collider.gameObject);
+
+            if (numLives <= 0)
+            {
+                win.text = "You Lose!!!";
+                madeBy.text = "Made by Marcus Napoloni";
+
+                Destroy(this.gameObject);
+            }
         }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Ground")
+        if (collision.collider.tag == "Ground" && isOnGround)
         {
             if (Input.GetKey(KeyCode.W))
             {
                 rb2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
             }
         }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
     }
 }
